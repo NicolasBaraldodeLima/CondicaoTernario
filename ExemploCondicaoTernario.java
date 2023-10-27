0cd7458b4d3097700e1d18c56b627255f1605c2a
Nicolas Baraldo <121053856+NicolasBaraldodeLima@users.noreply.github.com>
2023-10-27 17:47:58 -0300

    Add files via upload

---
+++ ExemploCondicaoTernario.java
@@ -0,0 +1,11 @@
+public class ExemploCondicaoTernario {
+
+    public static void main (String args[]) {
+        int numero1 = 10;
+        System.out.println(numero1 != 10 ? "é diferente de 10" : "é igual mesmo  10");
+
+
+        int numero2 = 10;
+        System.out.println(numero2 == 10 ? "é diferente de 10" : "é igual a 10");
+    }
+}
